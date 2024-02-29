2224efe0a740824f5ae8aaca32bdfda2c24447a7
Pasha147 <pavel147223@gmail.com>
2024-02-29 20:12:23 +0200

    Admin Link

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,35 +1,40 @@
 
-import styles from '@/app/(home)/home.module.css';
-import NewsArticles from '../ui/newsArticles';
-import { Suspense } from 'react';
-import SkelNewsArticles from '../ui/skeletons';
-import { Metadata } from 'next'
+
+import styles from "@/app/(home)/home.module.css";
+import NewsArticles from "../ui/newsArticles";
+import { Suspense } from "react";
+import SkelNewsArticles from "../ui/skeletons";
+import { Metadata } from "next";
+import Link from "next/link";
 
 
 export const metadata: Metadata = {
-  title: 'News',
-}
-
+  title: "News",
+};
 
 export default async function Home({
   searchParams,
 }: {
   searchParams?: {
-     page?: string;
+    page?: string;
   };
 }) {
-
   const currentPage = Number(searchParams?.page) || 1;
-
+  
   return (
-    <div className={styles.sectionHome +' container section'}>
-      <h1>{'Новини'}</h1>
+    <div className={styles.sectionHome + " container section"}>
+      <h1>{"Новини"}</h1>
+      <Link
+        key={'admin'}
+        href={'/admin'}
+        className={`${styles.navLink} }`}
+        
+      >
+        Admin
+      </Link>
       <Suspense fallback={<SkelNewsArticles />}>
-         <NewsArticles currentPage={currentPage}/>
-         
+        <NewsArticles currentPage={currentPage} />
       </Suspense>
     </div>
-  )
+  );
 }
-
-
